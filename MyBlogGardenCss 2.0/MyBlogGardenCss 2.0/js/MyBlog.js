6ac452ce417fb1defa5641047951bf99bffda0ba
祁新博 <qixinbo@91bihu.com>
2018-06-25 17:44:23 +0800

    提出骨架，准备写css

---
+++ MyBlogGardenCss 2.0/MyBlogGardenCss 2.0/js/MyBlog.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $("#navList").children("li").eq(4).append('<a class="menu" href="http://www.cnblogs.com/qixinbo/category/1012821.html">杂谈</a>');
+    $("#homepage_top_pager").remove();
+    $("#navList").children("li").eq(3).append('<a class="menu" href="http://www.cnblogs.com/qixinbo/category/1007682.html">C#文字</a>');
+    $("#navList").children("li").eq(2).append('<a class="menu" href="http://www.cnblogs.com/qixinbo/category/1007681.html">Javascript</a>');
+    $("#navList").children("li").eq(2).after('<li><a class="menu" href="http://www.cnblogs.com/qixinbo/category/1086024.html">数据结构</a></li>');
+    $(".catListTitle").hide();
+    $("#lnkBlogLogo").hide();
+});
