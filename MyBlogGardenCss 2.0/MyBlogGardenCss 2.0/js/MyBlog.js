534ff73b5d7740a8c18f875292f2f6bf110de726
祁新博 <qixinbo@91bihu.com>
2018-07-07 15:34:56 +0800

    目录js插件

--- MyBlogGardenCss 2.0/MyBlogGardenCss 2.0/js/MyBlog.js
+++ MyBlogGardenCss 2.0/MyBlogGardenCss 2.0/js/MyBlog.js
@@ -19,3 +19,115 @@
     $("#jumpA").children("a").addClass("shake-chunk").css("text-decoration", "none");
 
 });
+
+//目录组建
+var Dragging = function (validateHandler) { //参数为验证点击区域是否为可移动区域，如果是返回欲移动元素，否则返回null
+    var draggingObj = null; //dragging Dialog
+    var diffX = 0;
+    var diffY = 0;
+    function mouseHandler(e) {
+        switch (e.type) {
+            case 'mousedown':
+                draggingObj = validateHandler(e);//验证是否为可点击移动区域
+                if (draggingObj != null) {
+                    diffX = e.clientX - draggingObj.offsetLeft;
+                    diffY = e.clientY - draggingObj.offsetTop;
+                    e.preventDefault();
+                }
+                break;
+            case 'mousemove':
+                if (draggingObj) {
+                    draggingObj.style.left = (e.clientX - diffX) + 'px';
+                    draggingObj.style.top = (e.clientY - diffY) + 'px';
+                }
+                break;
+            case 'mouseup':
+                if (draggingObj != null) {
+                    $("a", "#box").toggle("fast", function () {
+                        if ($("#boxSideA").text() == "显示目录") {
+                            $("#boxSideA").text("").append('<span class="dot-top boxSideA"></span>');
+
+                        } else {
+                            $("#boxSideA").text("显示目录");
+                        }
+                    });
+                }
+                draggingObj = null;
+                diffX = 0;
+                diffY = 0;
+                break;
+        }
+    };
+    return {
+        enable: function () {
+            document.addEventListener('mousedown', mouseHandler);
+            document.addEventListener('mousemove', mouseHandler);
+            document.addEventListener('mouseup', mouseHandler);
+        },
+        disable: function () {
+            document.removeEventListener('mousedown', mouseHandler);
+            document.removeEventListener('mousemove', mouseHandler);
+            document.removeEventListener('mouseup', mouseHandler);
+        }
+    }
+}
+
+$(function () {
+    if ($("#cnblogs_post_body").children("h3").length > 1) {
+        $("body").append('<div id="box" class="box"><span id="boxSideA" class="boxSideA" onclick="">显示目录</span></div>');
+        $("#cnblogs_post_body").children("h3").each(function (i) {
+            $(this).attr("id", "chapter" + i);
+            $(this).css("font-weight", "bold");
+            var text = $(this).text();
+            $("#box").append('<br /><a class="navTitle" href="#chapter' + i + '" title="' + text + '">' + text + '</a>');
+        });
+        $("#box").append('<a></a>');
+        $("#box").children("a").first().addClass("Active");
+        Dragging(getDraggingDialog).enable();
+    }
+});
+
+//window.onscroll = function () {
+//    if ($(".day").length > 0 || $(".entrylistItem").length > 0) {
+//        return;
+//    }
+//    //already scrolled height $(document).scrollTop() is also can do this
+
+//    var maxCount = $(".navTitle").length - 1;
+//    var maxId = $(".navTitle").eq(maxCount).attr("href");
+//    maxId = maxId.substring(1, maxId.length);
+//    var maxHeight = $("#" + maxId).offset().top;
+//    for (var i = 0; i < $(".navTitle").length; i++) {
+//        var hrefTarget = $(".navTitle").eq(i).attr("href");
+//        hrefTarget = hrefTarget.substring(1, hrefTarget.length);
+//        //get element height
+//        var elementHeight = $("#" + hrefTarget).offset().top;
+//        var elementHeightAdd = elementHeight - 10;
+//        if (elementHeightAdd < nowScrolledHeight) {
+//            continue;
+//        } else {
+//            $(".Active").removeClass("Active");
+//            if (i == 0) {
+//                $(".navTitle").eq(0).addClass("Active");
+//            } else {
+//                $(".navTitle").eq(i - 1).addClass("Active");
+//            }
+//            break;
+//        }
+//    }
+//    if (nowScrolledHeight > (maxHeight - 10)) {
+//        $(".Active").removeClass("Active"); $(".navTitle").eq(maxCount).addClass("Active");
+//    }
+//}
+
+function getDraggingDialog(e) {
+    var target = e.target;
+    while (target && target.className.indexOf('boxSideA') == -1) {
+        target = target.offsetParent;
+    }
+    if (target != null) {
+        return target.offsetParent;
+    } else {
+        return null;
+    }
+}
